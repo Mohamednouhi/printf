2e9b26186860dbbf5b23a138c75111682ed8e771
Abdellatif Oudat <89777569+Aboud90@users.noreply.github.com>
2023-07-27 13:00:19 +0100

    Update specifier_exten.c

--- specifier_exten.c
+++ specifier_exten.c
@@ -38,3 +38,37 @@ int _print_percent(__attribute__((unused))va_list ls)
 	_wr_char('%');
 	return (1);
 }
+
+/**
+ * _print_int - to print a integer
+ * @args: arguments list
+ * Return: to return the amount of characters printed.
+ */
+int _print_int(va_list args)
+{
+	int number = va_arg(args, int);
+	char buffer[10];
+
+	/* Check if the number is negative.*/
+	
+	if (number < 0) 
+	{
+		_wr_char('-');
+		number = -number;
+	}
+	
+	/* Write the number to the buffer in decimal format.*/
+	
+	for (int i = 9; i >= 0; i--)
+	{
+		buffer[i] = (number % 10) + '0';
+		number /= 10;
+	}
+	
+	/*Write the buffer to stdout.*/
+	
+	for (int i = 0; i < 10; i++)
+	{
+		_wr_char(buffer[i]);
+	}
+}
